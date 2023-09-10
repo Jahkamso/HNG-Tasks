d1f8403b4cc1f841747e5d3412e6b86904122d44
Jahkamso <79403916+Jahkamso@users.noreply.github.com>
2023-09-11 00:48:31 +0100

    Add files via upload

---
+++ HNG-task-1/index.js
@@ -0,0 +1,19 @@
+const currentDay = document.getElementById("current-day")
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+const utcTime = new Date().getDay();
+const myCurrentday = days[utcTime]
+console.log("Current Day:", myCurrentday);
+currentDay.innerText = `Current day: ${myCurrentday}`;
+
+function Time() {
+    let displayTime = document.getElementById("display-time")
+    const utcTime = new Date();
+
+
+displayTime.innerText = utcTime.toLocaleTimeString()
+}
+
+Time()
+setInterval(Time, 1000)
+
