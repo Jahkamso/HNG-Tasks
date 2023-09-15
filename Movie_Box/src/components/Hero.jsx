6ab7e5accad6a9cc9b4e58708ee25e34e19d7f8c
Jahkamso <uzoekwejahkamso@gmail.com>
2023-09-15 21:59:55 +0100

    completed hero section

--- Movie_Box/src/components/Hero.jsx
+++ Movie_Box/src/components/Hero.jsx
@@ -1,27 +1,40 @@
 const Hero = () => {
   return (
-      <>
-          <div className="hero-container">
-              <header>
-                  <div className="logo-section">
-                      <img src="./logo.png" alt="" />
-                      <h2>MovieBox</h2>
-                  </div>
-                  <div className="input-section">
-                      <input type="text" placeholder="What do you want to watch" />
-                      <img src="/search.svg" alt="" />
-                  </div>
-                  <div className="sign-in">
-                      <a style={{ color: 'white' }}>Sign In</a>
-                      <div className="img-circle">
-                      <img src="/menu-icon.svg" alt="" />
-                      </div>
-                  </div>
-              </header>
-              <div className="hero-content">
-              <h1>John Wick</h1>
-              </div>
+    <>
+      <div className="hero-container">
+        <header>
+          <div className="logo-section">
+            <img src="./logo.png" alt="" />
+            <h2>MovieBox</h2>
           </div>
+          <div className="input-section">
+            <input type="text" placeholder="What do you want to watch" />
+            <img src="/search.svg" alt="" />
+          </div>
+          <div className="sign-in">
+            <a style={{ color: 'white' }}>Sign In</a>
+            <div className="img-circle">
+              <img src="/menu-icon.svg" alt="" />
+            </div>
+          </div>
+        </header>
+        <div className="hero-content">
+          <h1>
+            John Wick 3 : <br /> Parabellum
+          </h1>
+          <p>
+            John Wick is on the run after killing a member of the international
+            assassins' guild, and with a $14 million price tag on his head, he
+            is the target of hit men and women everywhere.
+                  </p>
+                  <div className="btn">
+                      <img src="/play.svg" alt="" />
+                      <button>
+                          Watch Trailer
+                      </button>
+                  </div>
+        </div>
+      </div>
     </>
   );
 };
